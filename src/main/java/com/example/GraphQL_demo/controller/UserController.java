adc82a2028ab74c6b314f7f2b90f74b97498f9a3
Owonin <Alxxxpr@yandex.ru>
2023-12-06 17:30:19 +0300

    User Pagination added

--- src/main/java/com/example/GraphQL_demo/controller/UserController.java
+++ src/main/java/com/example/GraphQL_demo/controller/UserController.java
@@ -4,12 +4,18 @@ import com.example.GraphQL_demo.entity.User;
 import com.example.GraphQL_demo.repositiry.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.awt.print.Pageable;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
@@ -34,6 +40,12 @@ public class UserController {
         log.info("Returning user with {} id", id);
         return userRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("User with id %d is not found", id)));
+    }
+
+    @QueryMapping Iterable<User> findAllUsersPageable(@Argument int size, @Argument int page){
+        log.info("Searching users with size {} and offset {}", size, page);
+
+        return userRepository.findAll(PageRequest.of(page,size));
     }
 
     @MutationMapping
